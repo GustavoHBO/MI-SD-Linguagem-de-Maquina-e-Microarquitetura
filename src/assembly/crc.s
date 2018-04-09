392f267c2978882a3151313af2ff75678eb6e68e
Gustavo Henrique <ghboliveira@hotmai.com>
2018-04-08 23:18:44 -0300

    Calculando CRC com palavras maiores que 32 bits

--- src/assembly/crc.s
+++ src/assembly/crc.s
@@ -6,6 +6,8 @@
 #		r11 - Armazena o polinômio de calculo do CRC
 #		r12 - Armazena o bit para identificação de necessidade de carry
 #		r13 - Armazena a quantidade de bits que ainda precisam ser calculados(Tamanho da mensagem - 32 + tamanho do polinômio do CRC)
+#		r14 - Buffer para ser carregado na mensagem
+#		r15 - Tamanho do polinômio
 #
 ###################################################################################################
 
@@ -17,7 +19,8 @@ data:
 	#movia r2, 0b1111111111001111 # Define os 16 bits menos significativos da mensagem em um registrador
 	#movhi r3, 0b0101100100101101 # Define os 16 bits mais significativos da mensagem em outro registrador
 	#add r10, r2, r3 # Junta os dois valores em apenas um registrador
-	movia r10, 0x592dffcf
+	movia r10, 0x592DFFCF
+	movia r14, 0x0
 	
 	# Definindo o polinômio, colocando-o no registador r11
 	movia r11, 0x04C11DB7
@@ -27,13 +30,15 @@ data:
 	
 	# Define a quantidade de bits que ainda precisam ser calculados(Tamanho da mensagem - 32 + tamanho do polinômio do CRC)
 	movia r13, 32
+	
+	movia r15, 32
 
 __main:
 	call __calcularCRC32
 
 __calcularCRC32:
 	
-	beq r0, r13, __fim
+	bgtu r0, r13, __fim
 	bltu r10, r12, __deslocarBit
 	slli r10, r10, 1 # Faz o carry de apenas um bit
 	subi r13, r13, 1 # Subtrai em 1 a quantidade de bits a serem calculados
@@ -43,6 +48,12 @@ __calcularCRC32:
 __deslocarBit:
 	slli r10, r10, 1 # Faz o carry de apenas um bit
 	subi r13, r13, 1 # Subtrai em 1 a quantidade de bits a serem calculados
+	bltu r15, r13, __deslocarBitMensagem
 	bgtu r13, r0, __calcularCRC32
+	call __fim
+	
+__deslocarBitMensagem:
+	bltu r14, r12, __calcularCRC32
+	addi r10, r10, 1
 	
 __fim:
