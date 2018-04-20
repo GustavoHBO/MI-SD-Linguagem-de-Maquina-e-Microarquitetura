bbc0f752b14ae0e427ea6bbf4cbee7db0d12f534
Gustavo Henrique <ghboliveira@hotmai.com>
2018-04-20 14:52:38 -0300

    Finalizado o conjunto de instruções para calculo do CRC32_Q no processador Nios(algumas modificações terão que ser feitas para executar no processador, pois foram executados somente testes no JNiosEmu. Além do controle da exibição terá que ser modificado a posição dos LED's e dos botões.)[Comentários atualizados]

--- src/assembly/crc.s
+++ src/assembly/crc.s
@@ -1,24 +1,32 @@
-####################################################################################################
-#				Documentação - README
-#
-#	Registradores:
-#		r8  - Aponta para o topo da pilha.
-#		r16 - Armazena a mensagem atual a ser utilizada no cálculo do CRC
-#		r17 - Buffer de entrada das novas mensagens
-#		r18 - Armazena o polinômio de calculo do CRC
-#		r19 - Armazena o bit para identificação de necessidade de carry
-#		r20 - Armazena a quantidade de bits que ainda precisam ser calculados(Tamanho da mensagem - 32 + tamanho do polinômio do CRC)
-#		r21 - Armazena a quantidade de bits do polinômio.
-#		r22 - Armazena a quantidade de bits a serem lidos do buffer
-#		r23 - Aponta para o endereço de memória a ser lido
-#
-###################################################################################################
+#########################################################################################################################################################
+#																			#
+#							Documentação - README										#
+#																			#
+#	- As instruções abaixo calculam o código CRC32_Q de uma mensagem qualquer.									#
+#																			#
+#	Registradores:																	#
+#		r8  - Aponta para o topo da pilha.													#
+#		r16 - Armazena a mensagem atual a ser utilizada no cálculo do CRC									#
+#		r17 - Buffer de entrada das novas mensagens												#
+#		r18 - Armazena o polinômio de calculo do CRC												#
+#		r19 - Armazena o bit para identificação de necessidade de carry										#
+#		r20 - Armazena a quantidade de bits que ainda precisam ser calculados(Tamanho da mensagem - 32 + tamanho do polinômio do CRC)		#
+#		r21 - Armazena a quantidade de bits do polinômio.											#
+#		r22 - Armazena a quantidade de bits a serem lidos do buffer										#
+#		r23 - Aponta para o endereço de memória a ser lido											#
+#																			#
+#########################################################################################################################################################
 
+#__________________________________________________________________ATENÇÂO__________________________________________________________________#
+#																	    #
+#	Caso deseje trocar o polinômio, altere o valor do registrador r21 para o tamanho correspondente ao polinômio.			    #
+#	Caso troque a mensagem, defina a quantidade de bit's a serem calculados no registrador r20. Pode ser necessário alterar o valor	    #
+#___________________________________________________________________________________________________________________________________________#
 
 .data
 	# Mensagem de 1000 bytes armazenada.
-	.word 0x6A92FD57, 0x48DAB0A1, 0x9BC8220E, 0xC6BE7975, 0xF50CF9B9, 0x4E1E541E, 0x51614047, 0x03BD9604, 0xDF9919A7, 0x7B0B247F, 0xFFEA9CC3, 0x41B62224, 0xA7A71587, 0xE0D4CCE1, 0xE41A93EA, 0x4140C7D4, 0x13E03024, 0xB78B92C7, 0x8F505A6E, 0x9E5E255A, 0x79339A7B, 0x2D778D3D, 0x8DE3FCF5, 0x085E7B19, 0x1F17A461, 0x424689E1, 0xA39CF261, 0x925FCBD8, 0xA8711739, 0x71565019, 0x5335CF40, 0x5902CCBD, 0xFA310458, 0xE86F0D83, 0xF8EFC364, 0x27D494C4, 0x80CF0801, 0x8B63D526, 0x85CE2FD2, 0x8345B356, 0xF158CD15, 0x5A5DDB0F, 0x0CEA6A58, 0x22669C41, 0xBA143E52, 0x9132A675, 0xABC15A66, 0xC135623F, 0x4973BF83, 0xA3DC665F, 0xD0761E96, 0x66F58199, 0xD0FCF769, 0x42128531, 0x7C88921C, 0x814E01E7, 0x3D5C3FD0, 0x17BD99CA, 0x3ABE1977, 0xD1EDB66D, 0x700BDDA6, 0xD0E2BC6A, 0x632D99FA, 0xE89342B0, 0x9471F614, 0x35E21F7D, 0x3BEC3BAF, 0x6C0FBAB2, 0x5D7A6066, 0xD5E776B3, 0x04ED7D6C, 0xF419B8C4, 0x1BC7FA4D, 0x51266FCD, 0x5E95B03E, 0x3911ED01, 0xF3CFE1A8, 0x755A0D87, 0x1B658E0A, 0x9091C0F6, 0x77A0C4E9, 0xF07F030C, 0x9A451B2C, 0xE7E76F42, 0x47A0D38E, 0xB79A457F, 0x15DDC46D, 0x92066DAA, 0x003F3457, 0xD89CBAED, 0x33C07F9C, 0xCB55ECB6, 0xF996961F, 0xD1DDDE20, 0xAD1E4722, 0x49E6858F, 0x936B7772, 0x2DC29AA2, 0xCE3766C9, 0xF799FB42, 0x179AD7BC, 0xE33D5A53, 0xF00FF2A2, 0xA3D6471D, 0xD2BE25A5, 0x36A4D69B, 0x96BC4316, 0x988B53A9, 0xAA81DC6D, 0xB2DA8F4D, 0x64E2BDA9, 0x217F3A87, 0x98B2FF85, 0xA474D2D8, 0xD6B78896, 0x37FC0EF3, 0x30EC206F, 0xF30089BC, 0xD1B12F84, 0x27229680, 0xF808F32F, 0xD2E1E7F9, 0xD6369AE4, 0x1424DE6B, 0x43A1E47D, 0x285D15E5, 0xD63E903F, 0x4BC2BD42, 0x17F63525, 0xB3376EA7, 0xBC410BD4, 0xC28B2F89, 0x4D822C77, 0x3F5E2A33, 0x3BACB675, 0xF87FEB72, 0xED93AFD7, 0xEE53A08B, 0xCA79FDCE, 0x23B1A704, 0x5D17FBB5, 0xE56020F3, 0xE47C27FE, 0x44F2E773, 0x44392BAE, 0x745C9C88, 0xAEA5D873, 0xEAFBFEA8, 0xCA524817, 0xF76B0388, 0x9D7247F3, 0xD0EACC81, 0xE723E038, 0xC16B1654, 0xCD060B85, 0xEB51414B, 0x5FF2BDAE, 0x96B0FF8F, 0x9AF378BA, 0xEB6FB2DA, 0xE548274F, 0x61D73B29, 0xA20BCC67, 0xCCA52D4E, 0x4550940D, 0xAE31CDB2, 0xECBFAF70, 0xC2AC8C2E, 0x475B66FC, 0xB731D292, 0x693749E4, 0xB50AA07A, 0xEB06BE22, 0xBEC5BDD5, 0x85BF78BE, 0x2AC8C203, 0x9D9ACC78, 0x39F40D89, 0xBB655338, 0xBC4F9DC9, 0x7F321075, 0xF4B32F3F, 0x17FD80C3, 0xA74C61AA, 0xCEC8D535, 0x9C03DB59, 0xE4406450, 0xB623767A, 0x2EC3D623, 0x24E14A48, 0x41464211, 0x3984E473, 0x2016DFD0, 0xC0713D00, 0x0AB1545E, 0xEB1B7204, 0x56B3975E, 0xFA5AC84C, 0x458FA62A, 0xDF30143C, 0xA98F58B8, 0x73D85B06, 0x24504149, 0x1DBAB616, 0xF7059685, 0x88430FBA, 0x384D1DDE, 0x6D36D61B, 0xCB65C202, 0xCBCFD45F, 0x99BAA09B, 0x56BE2BD5, 0xAFF007B9, 0x77A490EF, 0x61D3B68C, 0xBF773C9C, 0xADC80880, 0x12BD80EC, 0x461CF98F, 0x69D9DE19, 0x4BBDC296, 0x6F3D45F8, 0xA01EC778, 0xF43A0315, 0x44143B52, 0x7848DDEF, 0x1113E7FB, 0x0DC7E688, 0xBC69C973, 0x24D5DCFB, 0xCB28BF2E, 0x9E62DD40, 0x33BE5EDE, 0x7E55912E, 0x42AA71A7, 0x4F2C4B47, 0x9C9697A8, 0x40E31E06, 0x937091C1, 0x90C853C9, 0x805C1067, 0xF558FBB0, 0x770869BE, 0x37A846D7, 0x2DF52422, 0x9B234D0B, 0xEA36AEF5, 0xF384D654, 0x876F96A8, 0xC93CF1FE
-	
+	.word 0x54D1C7AF, 0x3489B031, 0x521672AA, 0x0AE6DD1C, 0x453332C3, 0x6778E4B7, 0xB8198BEC, 0x4D4854E3, 0xFCD2FA69, 0xDC8832E9, 0x0DD2ABE5, 0x399884FB, 0xD8A76E14, 0x17F522EF, 0xCAD7DBA4, 0xDDF196DB, 0x002A09FF, 0x7DE87EA7, 0x8DF61518, 0x7F5E7C83, 0xC7A1F73E, 0x8D3C2B0F, 0x0EBF806B, 0xE00C206F, 0xC4869119, 0xD0982FA5, 0x6B323A38, 0x9D118024, 0x691FBA22, 0xE5D08DC7, 0xDD5B565C, 0xE3487313, 0x5D37F12A, 0xEA3F737B, 0xB85879DB, 0xC1B07456, 0x3C1A6758, 0xA38BB898, 0xB5D5ECDF, 0x3EFA6AAF, 0x33106E65, 0xDB8FBD36, 0x9E69B096, 0x321F6488, 0x122A7FD3, 0x8BA57841, 0x183EB984, 0xB22967F6, 0x25CBF682, 0x056A7BA6, 0x8E251293, 0x446CECE8, 0x0528BB60, 0x94A65C86, 0x533BC38C, 0x10999ED5, 0xC7458275, 0x5673CF12, 0x1DE6819D, 0x206E7BD0, 0xC92CD4CD, 0xD57EFC20, 0x6A9DDB87, 0xE5EC005B, 0x8C2BE0A2, 0x076BE4B9, 0xD297BAB5, 0x029865FE, 0x995115CC, 0xDA74744A, 0xD9A8E5B3, 0xD7148B32, 0xB3A2387A, 0x2D729CFA, 0x1E595AE2, 0xCB8816C9, 0x9479A65D, 0x393BE900, 0x318D958C, 0x520838A8, 0xA5EC2B7E, 0x352C8909, 0x5761596E, 0xCC17F9E8, 0x192C2640, 0xDF5A2FEE, 0x65600CEA, 0x93AF653D, 0x986E8385, 0x14C46466, 0x820D5BB1, 0x195CD416, 0xD5D7D6E8, 0x261EDCAC, 0xB4FED589, 0xB9322611, 0x109E8601, 0xF9D5B76A, 0x68DC1A00, 0x668BBA2A, 0x51BDAC3F, 0x1AE080E6, 0xD4669A5E, 0x8D836B85, 0xBAE8FF9F, 0xAE5F2507, 0xA0993935, 0x7FAC815A, 0xB123C6B1, 0xE6F4A1EF, 0x14FEBDC0, 0x381E7357, 0x78B2D706, 0x1E925FD1, 0xAF6339A3, 0x30E9D9BF, 0x29979C6C, 0xB4B4D700, 0xDE5BEB54, 0x1FE8ADC2, 0x65BE83CF, 0xBB8DCE4C, 0xB916E963, 0x5D059755, 0xB236DCAD, 0x78C53E2C, 0x46850769, 0xA4C44C61, 0x48C29F0F, 0x03F47097, 0xE4543093, 0x427C7E17, 0x27577D78, 0xDEFF610F, 0x453A5660, 0x215C9C78, 0xBD5A6AA3, 0x9AA9C7DE, 0x72F6FC35, 0x6B462138, 0xDE3F311B, 0x5783BFDD, 0x42156A29, 0xCF528EA5, 0x667C5169, 0xF01338F2, 0x45F75330, 0x7CF0DCB7, 0x28D518C9, 0xDFD3F3F2, 0x12CE5243, 0x4BED06C9, 0x6FD559AC, 0xBD04F84B, 0x76A99906, 0x82B42C1F, 0xED452A15, 0xB0BC30A9, 0x66759690, 0xC488FB10, 0xD545B37E, 0xBBC0111C, 0xB4CE11C2, 0x91592E63, 0x73A96313, 0x60B3B0E0, 0x43F7FA06, 0xC5138D18, 0x54338A0F, 0xE425D76C, 0xF930369D, 0x98BE8BA0, 0xB5ECD03D, 0x87387B89, 0x0404ACFD, 0x26137D61, 0x7F1CBA36, 0xD0EBCC69, 0xDB301971, 0x448A3E7C, 0x23DC63AD, 0x513BDB5D, 0xFB5ECD3B, 0xDC0B3C57, 0xF5563055, 0x1EF22DA9, 0x34754938, 0xC467B77B, 0x0B42911C, 0x5EFB2A81, 0x56171C5D, 0x5FBCEFAD, 0xFC8EFDC2, 0x9B5056BD, 0x8C052D9B, 0x65A33D76, 0xB7E118C2, 0x60FBE470, 0xD497F7E2, 0x0F138AFC, 0x33952843, 0x421AC2CA, 0x8B4361A0, 0x417C198C, 0xDD98DC8F, 0x4BBEA9DC, 0xEB0F7BEC, 0x2FC07ED0, 0x3283F944, 0x1BA0BB1F, 0x1F7A584F, 0x5C18026C, 0xB9E23803, 0x817962C3, 0x5558E78D, 0x56548A52, 0xF974458B, 0xB9F49591, 0x3BAF0C28, 0xB09BE3BC, 0x119D134B, 0x59A5EC39, 0xC1388DD5, 0x1E0EB098, 0x82504F85, 0x2726AF98, 0x69B7C611, 0xB92CDDA2, 0x49D377B5, 0x92C32E8B, 0x2333E382, 0xD38DCF10, 0xB658B4E6, 0x19DB44F1, 0x95D6D6C1, 0xE733FB8C, 0x522056A5, 0xF9C6F6FB, 0xFBF09978, 0xF952430F, 0x3A01FF3D, 0x15A32EF2, 0x644A5CCB, 0x6D24830E, 0x83A461F9, 0x22409E20, 0x4F580045, 0x7B9A55C9, 0x643AACF3, 0x4A88B580	
+		
 		# Declaração dos valores a serem utilizados
 	
 	# Definindo o topo da pilha
@@ -39,23 +47,24 @@
 	# Carrega os registradores de mensagem e buffer a partir da memória.
 	
 	movia r23, 0x30000 # Define o endereço inicial de leitura.
-	addi r22, r0, 32 # Define o buffer com tamanho 32.
+	add r22, r0, r0 # Define o buffer como 0 bit's carregados.
 	
 	ldw r16, 0(r23) # Carrega os primeiros 32 bit's na mensagem.
-	addi r23, r23, 4
-	ldw r17, 0(r23) # Carrega mais 32 bit's no buffer.
+	
 .text
 
+	# Executa o procedimento inicial.
 __main:
 	call __calcularCRC32
 
+	# Calcula o código CRC da mensagem armanenada no registrador r16 com o polinômio r18.
 __calcularCRC32:
-	
 	bgeu r0, r20, __exibirCodigo # Verifica se ainda existe mensagem a ser calculada. Caso r0 seja maior que r20.
 	call __prepararMensagem # Chama o procedimento para deslocar os bit's se necessário.
 	xor r16, r16, r18 # Executa o XOR da mensagem com o polinômio.
 	br __calcularCRC32 # Chama novamente o procedimento de calcular CRC32.
 	
+	# Prepara a mensagem para que seja feita a operação XOR da mensagem com o polinômio.
 __prepararMensagem:
 	bgt r0, r20, __exibirCodigo # Verifica se ainda existe mensagem a ser calculada. Caso r0 seja maior que r20.
 	bgtu r19, r16, __deslocarBit # Verifica se o bit mais significativo da mensagem é 0. Caso r19 seja maior ou igual a r16.
@@ -66,6 +75,7 @@ __prepararMensagem:
 	ldw r31, 0(r8)
 	ret
 	
+	# Desloca o bit mais significativo da mensagem, carregando outro no menos significativo(dependendo do valor de r7).
 __deslocarBit:
 	slli r16, r16, 1 # Desloca um bit para a esquerda.
 	subi r20, r20, 1 # Decrementa a quantidade de bit's a serem lidos.
@@ -75,22 +85,26 @@ __deslocarBit:
 	beq r8, r4, __retornar # Verifica se deve retornar a chamada da instrução call no conjuto de intruções __calcularCRC32
 	br __prepararMensagem
 
+	# Verifica se o bit a ser carregado na mensagem é 1 ou 0 e se é necessário carregar novamente o buffer.
 __deslocarBuffer:
 	bgeu r0, r22, __carregarMensagem # Verifica se ainda tem mensagens a serem lidas no buffer. Caso r0 seja maior ou igual a r22.
 	bgtu r19, r17, __deslocarUmBit # Verifica se o bit a ser deslocado é 1 ou 0. Caso r19 seja maior que r17.
 	addi r16, r16, 1
 	br __deslocarUmBit
 
+	# Desloca um bit para esquerda do buffer.
 __deslocarUmBit:
 	slli r17, r17, 1 # Desloca para a esquerda um bit de r17.
 	subi r22, r22, 1 # Decrementa em um bit a quantidade de bit's a serem lidos do buffer.
 	movia r4, 0x3FF84
 	beq r8, r4, __retornar
 	br __prepararMensagem
-
+	
+	# Label de retorno
 __retornar:
 	ret
 
+	# Carrega a mensagem da memória para o registrador buffer.
 __carregarMensagem:
 	sub r20, r20, r22 # Decrementa a quantidade de bits do buffer.
 	addi r23, r23, 4 # Incrementa a posição de leitura dos próximos bytes a serem recuperados da memória.
@@ -99,24 +113,30 @@ __carregarMensagem:
 	addi r22, r0, 32 # Quantidade de bit's recuperados da memória.
 	br __deslocarBuffer # Vai para o conjunto de instruções de deslocar bit's do buffer.
 
+	# Determina a quantidade de bits que faltam ser calculados. Caso esse valor seja menor que 32.
 __quantidadeBitLer:
 	mov r22, r20
 	br __deslocarBuffer # Vai para o conjunto de instruções de deslocar bit's do buffer. 
 	
+	# Exibe o CRC calculado nos 4 LEDs, intercalando-os com os botões. O botão mais significativo encerra o processo.
 __exibirCodigo:
 	mov r4, r16 # Copia o valor para r4.
-	addi r5, r0, 0x840
-	addi r6, r0, 0x10
-	addi r7, r0, 0x810
-	loop1:
-		ldw r8, 0(r5)
-		bgtu r8, r0, loop2
-		br loop1
-	loop2:
-		ldw r8, 0(r5)
-		bgtu r8, r0, loop2
-		roli r4, r4, 0x4
-		stw r4, 0(r7)
-		br loop1
+	addi r5, r0, 0x840 # Define o endereço de memória no qual os botões escrevem.
+	addi r6, r0, 0x8 # Define o valor no qual o botão irá escrever na memória como fim de execução.
+	addi r7, r0, 0x810 # Define o endereço de memória correspondente aos LEDs.
+	add r8, r0, r0 # Zera o valor de r8. Registrador que armazena o atual valor dos botões.
+	addi r9, r0, 0x4 # Determina a quantidade de bit's a serem representados por vez.
+	loop1: # Loop1 aguarda o botão ser pressionado.
+		ldw r8, 0(r5) # Carrega a mensagem gravada pelo botão na memória em r5
+		beq r8, r6, __fim # Caso o bit mais significativo da mensagem seja 1.
+		bgtu r8, r0, loop2 # Caso o botão tenha sido pressionado.
+		br loop1 # Retorna ao loop1.
+	loop2: # Aguarda o botão ser solto
+		ldw r8, 0(r5) # Carrega a mensagem gravada pelo botão na memória em r5
+		beq r8, r6, __fim # Caso o bit mais significativo da mensagem seja 1.
+		bgtu r8, r0, loop2 # Caso o botão não tenha sido solto.
+		stw r4, 0(r7) # Grava a mensagem na posição de leitura correspondente aos LEDs.
+		ror r4, r4, r9 # Desloca a mensagem à direita para exibir nos LEDs.
+		br loop1 # Retorna o fluxo para o loop1.
 
-__fim:
+__fim: # Fim do código.
