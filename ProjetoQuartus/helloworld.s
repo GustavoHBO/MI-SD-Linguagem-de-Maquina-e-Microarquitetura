f6bc3bdb045fde140752b8096edf3ccf2a4caa80
Victor Munduruca <victormunduruca@gmail.com>
2018-05-04 00:20:34 -0300

    Organizando e Adicionando README

---
+++ ProjetoQuartus/helloworld.s
@@ -0,0 +1,124 @@
+.data
+	# Mensagem de 1000 bytes armazenada.
+msg:	.word 0x79A80418, 0x76885397, 0x420841BF, 0x7976AC1A, 0x59F1FC4A, 0xC7687BC2, 0xD7A9C795, 0x82FAA9AB, 0x090016FD, 0x83A9C3CA, 0xD1319B02, 0x9F3C1009, 0x4827E444, 0xE0607276, 0xC6514AB6, 0x8534E48C, 0xA2C9D5A8, 0x04271A20, 0x05B49E39, 0xD8985D6A, 0x275EE11F, 0xE1714738, 0x29DE7298, 0xCB702D4E, 0xB7DC99E3, 0xB42A46FA, 0x25A3A187, 0x05A3EB26, 0x391426DF, 0x76601D0B, 0x92242A7C, 0xD8E9F0EB, 0x0BEB024C, 0x6092C8F6, 0xEC027556, 0x5E840D70, 0xB2DD074D, 0xC96E5509, 0x05B270E2, 0x1B30AE7D, 0x2225F1BE, 0xA232672A, 0x49F24E2E, 0x47FBD659, 0xACC9E19C, 0x9267ABFA, 0xAEBC9B1C, 0x8520A688, 0x3E553852, 0xE0293209, 0x57653D85, 0xE4DD9453, 0xC8D0D74A, 0xDC06E3DC, 0x5C9107F3, 0x9D245164, 0x866EC747, 0xA85D7EEE, 0xC9C4300D, 0xE5FE86DB, 0x9D4BA910, 0xCD285CFF, 0x7E7E986D, 0x1458EE90, 0xEAF4A178, 0x03A4D2B1, 0x2877C6E9, 0x4994A3E3, 0x85B1BBF6, 0x06438A08, 0x82736B0A, 0x9C796714, 0xA2562DD9, 0x902ABA33, 0x602CF5FB, 0xEB122D14, 0xF973ACFF, 0xD743F512, 0x05706187, 0x35C0121E, 0x8B5E9329, 0x14D8C0F6, 0xF3E5D103, 0x820F4F0E, 0x18867F08, 0x1B8D56EA, 0xF27F9F63, 0xCB6210B5, 0x74B8F5E3, 0x65932E40, 0x6ADF16D8, 0xF9759560, 0x284B2FFF, 0xFFC0CEF6, 0x5E0D80B4, 0x5A9FE655, 0xABBCC1D6, 0x7C3A076B, 0x464D30BC, 0x31670562, 0x5D7F93D1, 0x05B10C42, 0xE0373CE2, 0x78CF11F7, 0x345381A0, 0xDD5DCAA2, 0x928923B3, 0x23BE7BA3, 0x4926AE69, 0x57109226, 0x93C46E45, 0x8B23D5DB, 0x34F8267F, 0x7B614EF1, 0x55F132EC, 0xA2C53C87, 0x25F0CDBF, 0x3561D617, 0x8D638B8B, 0xA8EF420C, 0xA46AD350, 0x8A725CA1, 0xFC58EEF9, 0xC937AF3E, 0xDA36D727, 0xD83F9E26, 0x054A6994, 0xA1769D01, 0x70A1AD35, 0x739FBB9B, 0x30129FC4, 0xBD8521C5, 0x315F5FEC, 0xF348C015, 0xC1EF7224, 0xBEBF8C48, 0x3885843C, 0xB595AC6A, 0x119655FD, 0xABE0F17F, 0x3EF9E643, 0xF9FC3067, 0x353C54EA, 0x790FB39F, 0xD01D972A, 0x45130ADE, 0xF7F4B96F, 0xAED48A47, 0x78F3C261, 0xC96895F0, 0x68503149, 0x8EE5265C, 0xCF5B6C8F, 0x95786926, 0x44CB050C, 0xC6943B75, 0x3FA082C7, 0x69DDEA56, 0x6BDFD627, 0x4B8B915D, 0xC1C4D1DB, 0x57C086E3, 0xFB71752B, 0x67AFA2DB, 0x9E3E2E5C, 0xCA2D05FD, 0xC7EC5EFD, 0x1A10556A, 0x870268FB, 0xB50AE0EF, 0x66D0193A, 0x5201DCCD, 0xBBABFDD1, 0xF94E38F4, 0xF18069E8, 0xCA104E99, 0xD58A8A7A, 0xB9CF8D54, 0x20A8E31C, 0x078D2F60, 0x30413995, 0xE0FABCE9, 0x3322821E, 0xA5A42B4A, 0xE1990D24, 0x200D8BA1, 0x6370E732, 0x4711929D, 0x7A118BA1, 0xFAAE1B5A, 0x476A1AF3, 0xD3D7ACC8, 0xF54E2415, 0x09467E59, 0xCCD62B7C, 0xF96406C3, 0xA716A860, 0x58F81318, 0x3673A184, 0xBC3928AD, 0x2B97B3C7, 0xF5300C7E, 0xD6587E07, 0xC2F0EDFB, 0x0098AF38, 0x2EE1E42A, 0xC8D4BBC8, 0xC0B6B737, 0xA8A3D451, 0x56209B5D, 0x3C702622, 0x6DBB242C, 0xA2769766, 0x0FFD7012, 0xC65F5014, 0xA792CF60, 0x53C113F0, 0x5048E402, 0xAFFC2053, 0xD19C755B, 0x07003D0F, 0xA48F3B0C, 0x66D9A73C, 0xF32DFC87, 0x560FF894, 0xC31D7FB1, 0xD65E14BA, 0xFF70F54C, 0x218B9C91, 0x303B593B, 0x0AC99819, 0xF07C500C, 0x91122FEF, 0x299C1116, 0x0FAC917C, 0xEBF39444, 0xBD683C87, 0x77558DD0, 0x9E4A3415, 0xD10E3974, 0x1A11C46D, 0x614FB34C, 0xFA9E766E, 0x98E72478, 0x3CEC3743, 0x54B13C5B, 0xC4AA4E77, 0x1635B018, 0xDE1AA92B, 0xBE991D06
+.text
+	# Declaração dos valores a serem utilizados
+	
+	# Definindo o topo da pilha
+	movia r8, 0x1FBF
+
+	# Definindo o polinômio, colocando-o no registador r18
+	movia r18, 0x814141AB
+	
+	# Utilizado para identificar se o primeiro bit é 1
+	movhi r19, 0b1000000000000000 
+	
+	# Define a quantidade de bits que ainda precisam ser calculados(Tamanho da mensagem - 32 + tamanho do polinômio do CRC)
+	addi r20, r0, 8000
+	
+	# Define a quantidade de bits do polinômio
+	addi r21, r0, 32
+	
+	# Carrega os registradores de mensagem e buffer a partir da memória.
+	
+	movia r23, msg # Define o endereço inicial de leitura.
+	add r22, r0, r0 # Define o buffer como 0 bit's carregados.
+	
+	ldw r16, 0(r23) # Carrega os primeiros 32 bit's na mensagem.
+	
+
+
+	# Executa o procedimento inicial.
+.global _start 
+_start:
+	call __calcularCRC32
+
+	# Calcula o código CRC da mensagem armanenada no registrador r16 com o polinômio r18.
+__calcularCRC32:
+	bgeu r0, r20, __exibirCodigo # Verifica se ainda existe mensagem a ser calculada. Caso r0 seja maior que r20.
+	call __prepararMensagem # Chama o procedimento para deslocar os bit's se necessário.
+	xor r16, r16, r18 # Executa o XOR da mensagem com o polinômio.
+	br __calcularCRC32 # Chama novamente o procedimento de calcular CRC32.
+	
+	# Prepara a mensagem para que seja feita a operação XOR da mensagem com o polinômio.
+__prepararMensagem:
+	bge r0, r20, __exibirCodigo # Verifica se ainda existe mensagem a ser calculada. Caso r0 seja maior que r20.
+	bgtu r19, r16, __deslocarBit # Verifica se o bit mais significativo da mensagem é 0. Caso r19 seja maior ou igual a r16.
+	stw r31, 0(r8)
+	addi r8, r8, 4 # Incrementa o ponteiro para o topo da pilha.
+	call __deslocarBit
+	subi r8, r8, 4 # Decrementa o ponteiro para o topo da pilha.
+	ldw r31, 0(r8)
+	ret
+	
+	# Desloca o bit mais significativo da mensagem, carregando outro no menos significativo(dependendo do valor de r7).
+__deslocarBit:
+	slli r16, r16, 1 # Desloca um bit para a esquerda.
+	subi r20, r20, 1 # Decrementa a quantidade de bit's a serem lidos.
+	addi r4, r0, 0b100000
+	bgeu r20, r4, __deslocarBuffer # Verifica se os bit's a serem deslocados são da mensagem ou do polinômio.
+	movia r4, 0x1FC3
+	beq r8, r4, __retornar # Verifica se deve retornar a chamada da instrução call no conjuto de intruções __calcularCRC32
+	br __prepararMensagem
+
+	# Verifica se o bit a ser carregado na mensagem é 1 ou 0 e se é necessário carregar novamente o buffer.
+__deslocarBuffer:
+	bgeu r0, r22, __carregarMensagem # Verifica se ainda tem mensagens a serem lidas no buffer. Caso r0 seja maior ou igual a r22.
+	bgtu r19, r17, __deslocarUmBit # Verifica se o bit a ser deslocado é 1 ou 0. Caso r19 seja maior que r17.
+	addi r16, r16, 1
+	br __deslocarUmBit
+
+	# Desloca um bit para esquerda do buffer.
+__deslocarUmBit:
+	slli r17, r17, 1 # Desloca para a esquerda um bit de r17.
+	subi r22, r22, 1 # Decrementa em um bit a quantidade de bit's a serem lidos do buffer.
+	movia r4, 0x1FC3
+	beq r8, r4, __retornar
+	br __prepararMensagem
+	
+	# Label de retorno
+__retornar:
+	ret
+
+	# Carrega a mensagem da memória para o registrador buffer.
+__carregarMensagem:
+	sub r20, r20, r22 # Decrementa a quantidade de bits do buffer.
+	addi r23, r23, 4 # Incrementa a posição de leitura dos próximos bytes a serem recuperados da memória.
+	ldw r17, 0(r23) # Ler da memória os 32 bit's.
+	bgtu r21, r20, __quantidadeBitLer # Verifica se a quantidade de bit's a serem lidos na memória é menor que 32.
+	addi r22, r0, 32 # Quantidade de bit's recuperados da memória.
+	br __deslocarBuffer # Vai para o conjunto de instruções de deslocar bit's do buffer.
+
+	# Determina a quantidade de bits que faltam ser calculados. Caso esse valor seja menor que 32.
+__quantidadeBitLer:
+	mov r22, r20
+	br __deslocarBuffer # Vai para o conjunto de instruções de deslocar bit's do buffer. 
+	
+	# Exibe o CRC calculado nos 4 LEDs, intercalando-os com os botões. O botão mais significativo encerra o processo.
+
+__exibirCodigo: 
+		movia r12, 0x3010
+		movia r14, 0x3000
+		movia r5, 900000 
+		movi r6, 1
+		movi r8, 4
+
+checando_botao:		
+		ldb r9, 0(r14)
+		beq r9, r6, exibe_dados
+		br checando_botao
+
+exibe_dados:	
+		stb r16, 0(r12)
+		ror r16, r16, r8 
+		movia r5, 900000 
+		call delay
+
+delay:		
+		subi r5, r5, 1
+		bne r5, r0, delay
+		call checando_botao
+
+end:
+	br end
+	.end
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# MI-SD-Linguagem-de-Maquina-e-Microarquitetura
+# Compilando o projeto
+
+
+Para compilar e executar o programa na placa FPGA realize os seguintes passos:
+
+- Abra o arquivo .qpf dentro da pasta do projeto. 
+- Em seguida abra o Altera Monitor
+- Selecione “Abrir um projeto” . Navegue até o arquivo monitor.ncf dentro do diretório do projeto
+- Vá até o Quartus 13.0sp1 e compile o projeto
+- Ao final da compilação, abra o altera monitor novamente, clique em compilar 
+- Clique em “Actions -> Convert Program o Hex File”
+- Abra o quartus novamente. A seguir, clique em “Processing -> Update Memory Inicialization File”
+- Ao final do processo clique duas vezes sobre a “Task”, “Assembler” e em “Sim”na caixa de diálogo.
+- Programe a placa
+
+Para testar novos valores, recomendamos a utilização da Calculadora presente na raiz do repositório. Ao gerar uma nova mensagem de 1KB, é necessário somente copiar e colar no código helloworld.s e realizar o processo de compilação novamente.
